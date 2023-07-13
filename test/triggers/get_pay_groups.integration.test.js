639686c16f35cb353ee733e759630990cfb2d0fe
Craig Buchanan <craig.buchanan@cogniza.com>
2023-07-13 12:26:15 +0000

    adding integration tests

---
+++ test/triggers/get_pay_groups.integration.test.js
@@ -0,0 +1,38 @@
+const zapier = require('zapier-platform-core');
+
+const App = require('../../index');
+const appTester = zapier.createAppTester(App);
+zapier.tools.env.inject();
+
+beforeAll(() => {
+  // TODO: get a new access token
+});
+
+describe('Trigger - get_paygroups', () => {
+
+  it('should get an array', async () => {
+
+    // arrange
+    const bundle = {
+      authData: {
+        subdomain: process.env.SUBDOMAIN,
+        access_token: process.env.ACCESS_TOKEN,
+        subscription_key: process.env.SUBSCRIPTION_KEY,
+        legal_entity_id: process.env.LEGAL_ENTITY_ID,
+      },
+    };
+
+    // act
+    const results = await appTester(
+      App.triggers['get_paygroups'].operation.perform,
+      bundle
+    );
+
+    // console.log('results',results)
+  
+    // assert
+    expect(results.length).toBeGreaterThan(0)
+
+  });
+
+});
