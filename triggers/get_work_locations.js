d2db749dafe72ae69a5acc08db03b912472075de
Craig Buchanan <craig.buchanan@cogniza.com>
2023-07-13 12:31:58 +0000

    adding subdomain support

--- triggers/get_work_locations.js
+++ triggers/get_work_locations.js
@@ -1,6 +1,6 @@
 const perform = async (z, bundle) => {
   const options = {
-    url: `https://apis.paycor.com/v1/legalentities/${bundle.authData.legal_entity_id}/worklocations`,
+    url: `https://${bundle.authData.subdomain}.paycor.com/v1/legalentities/${bundle.authData.legal_entity_id}/worklocations`,
     method: 'GET',
     headers: {
       Accept: 'application/json',
@@ -21,7 +21,9 @@ module.exports = {
   operation: {
     perform: perform,
     canPaginate: false,
-    outputFields: [{ key: 'id' }, { key: 'name' }],
+    outputFields: [
+      { key: 'name' }
+    ],
     sample: {
       Id: "5245ae3d-e570-0000-0000-000066000000",
       Name: "ACME East is an open concept office.",
