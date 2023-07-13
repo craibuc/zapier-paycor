a05d8a15585ad4d67e028030d4086b06242ffb90
Craig Buchanan <craig.buchanan@cogniza.com>
2023-07-13 12:42:20 +0000

    adding mocking

--- test/triggers/get_work_locations.test.js
+++ test/triggers/get_work_locations.test.js
@@ -4,6 +4,8 @@ const App = require('../../index');
 const appTester = zapier.createAppTester(App);
 zapier.tools.env.inject();
 
+const nock = require('nock');
+
 describe('Trigger - get_work_locations', () => {
 
   it('should get an array', async () => {
@@ -16,6 +18,51 @@ describe('Trigger - get_work_locations', () => {
         legal_entity_id: process.env.LEGAL_ENTITY_ID,
       },
     };
+
+    const response = {
+      "hasMoreResults": false,
+      "continuationToken": null,
+      "additionalResultsUrl": null,
+      "records": [
+          {
+              "id": '616aca68-6543-451a-8eff-46959c70376b',
+              "name": "Acme, Inc.",
+              "addresses": [
+                  {
+                      "type": "Mailing",
+                      "streetLine1": "1234 Maple Street",
+                      "streetLine2": null,
+                      "suite": null,
+                      "city": "Minneapolis",
+                      "state": "MN",
+                      "country": "USA",
+                      "county": "Hennepin",
+                      "zipCode": "12345"
+                  },
+                  {
+                      "type": "Physical",
+                      "streetLine1": "1234 Maple Street",
+                      "streetLine2": null,
+                      "suite": null,
+                      "city": "Minneapolis",
+                      "state": "MN",
+                      "country": "USA",
+                      "county": "Hennepin",
+                      "zipCode": "12345"
+                  }
+              ],
+              "phones": [],
+              "locationNumber": null,
+              "storeId": "Acme, Inc.",
+              "isFmlaEligible": true
+          }
+      ]
+    }
+
+    // mocks the next request that matches this url and body
+    nock(`https://${bundle.authData.subdomain}.paycor.com/v1`)
+      .get(`/legalentities/${bundle.authData.legal_entity_id}/worklocations`)
+      .reply(200, response);
 
     // act
     const results = await appTester(
