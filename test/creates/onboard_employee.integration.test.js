224936b1d33bfd54fd40ceb5d1b9e8d0c9fa25cc
Craig Buchanan <craig.buchanan@cogniza.com>
2023-07-14 16:12:04 +0000

    fixing tests

--- test/creates/onboard_employee.integration.test.js
+++ test/creates/onboard_employee.integration.test.js
@@ -2,13 +2,14 @@ const zapier = require('zapier-platform-core');
 
 const App = require('../../index');
 const appTester = zapier.createAppTester(App);
+zapier.tools.env.inject();
 
 beforeAll(() => {
   // TODO: get a new access token
 });
 
 describe('Create - onboard_employee', () => {
-  zapier.tools.env.inject();
+  
 
   it('should create an object', async () => {
 
@@ -20,9 +21,7 @@ describe('Create - onboard_employee', () => {
         subscription_key: process.env.SUBSCRIPTION_KEY,
         legal_entity_id: process.env.LEGAL_ENTITY_ID,
       },
-
       inputData: {
-          // ExportedByEmailAddress: "email@domain.com",
           FirstName: "Edwin",
           LastName: "Hubble",
           PreferredName: "Carroll",
@@ -41,8 +40,8 @@ describe('Create - onboard_employee', () => {
           HomeEmailAddress: "homeEmail@domain.com",
           StartDate: "2019-11-01T00:00:00Z",
           JobTitle: "Software Engineer",
-          WorkLocationId: "dc069074-24b2-0000-0000-000014e00100",
-          DepartmentCode: 80,
+          WorkLocationId: "1c694b5b-7129-491c-a0e2-2161d9e65d71",
+          DepartmentCode: 10,
           BaseSalary: 10000,
           SalaryFrequency: "Bi-Weekly",
       },
@@ -53,6 +52,8 @@ describe('Create - onboard_employee', () => {
       App.creates['onboard_employee'].operation.perform,
       bundle
     );
+
+    console.log('result',result)
 
     // assert
     expect(result).toHaveProperty('id');
